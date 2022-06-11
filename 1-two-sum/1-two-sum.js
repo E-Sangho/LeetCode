ae4c7092cd0ed35a48c613346caba743087ca2d2
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-06-11 14:32:51 +0900

    Time: 104 ms (60.18%), Space: 43.5 MB (22.03%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const n = nums.length;
+    let map = new Map();
+    
+    for (let i = 0; i < n; ++i) {
+       if (map.has(target - nums[i])) {
+           return [map.get(target - nums[i]), i];
+       }
+       map.set(nums[i], i);
+    }
+};
