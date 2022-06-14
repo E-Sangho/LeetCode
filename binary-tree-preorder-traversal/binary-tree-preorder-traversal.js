5e723466a3780cb0e18f53ec957d88ae0fc52f03
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-06-14 10:35:45 +0900

    Time: 75 ms, Memory: 42.3 MB - LeetHub

---
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if (root === null) {
+        return []; 
+    }
+    
+    if (root.left === null && root.right === null) {
+        return [root.val];
+    }
+    
+    let left = preorderTraversal(root.left);
+    let right = preorderTraversal(root.right);
+   
+    return [root.val, ...left, ...right];
+};
