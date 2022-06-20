6b001f9b065fdb2cbb92ba2da027cc4daa75a0c0
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-06-20 13:48:10 +0900

    Time: 174 ms, Memory: 44.9 MB - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -12,28 +12,22 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-    if (postorder.length === 0) {
-        return null;
-    }
-    let root = postorder.pop();
-    let rootIndex = inorder.findIndex(e => e === root);
-    let leftInorder = inorder.slice(0, rootIndex);
-    let rightInorder= inorder.slice(rootIndex + 1);
-    let leftPostorder = postorder.slice(0, leftInorder.length);
-    let rightPostorder = postorder.slice(leftInorder.length, postorder.length);
+    let N = postorder.length;
     
-    let left = buildTree(leftInorder, leftPostorder);
-    let right = buildTree(rightInorder, rightPostorder);
-    
-    let rootNode = new TreeNode(root, left, right);
-    return rootNode;
+    let answer = Recursive(0, N - 1, 0, N - 1);
+
+    return answer;
+
+    function Recursive(is, ie, ps, pe) {
+        if (ps > pe) {
+            return null;
+        } 
+        let root = postorder[pe];
+        let rootIndex = inorder.findIndex(e => e === root);
+
+        let left = Recursive(is, rootIndex - 1, ps, ps + rootIndex - is - 1);
+        let right = Recursive(rootIndex + 1, ie, ps + rootIndex - is, pe - 1);
+        let newNode = new TreeNode(root, left, right);
+        return newNode;
+    }  
 };
-/*
- inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
-    root = 3, [9], [15, 20, 7],  [9], [15, 7, 20]
-    root = 9 [], []
-    root = 20 [15], [7], [15], [7]
-    root = 15 [], []
-    root = 7 [], [] 
-        
-*/
