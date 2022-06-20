a80ac114b12ea22233104872e7dc48c40336790b
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-06-20 14:11:18 +0900

    Time: 129 ms, Memory: 45.1 MB - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -12,11 +12,11 @@
  * @return {TreeNode}
  */
 var buildTree = function(inorder, postorder) {
-    let Indexer = new Map();
+    let Indexer = {};
     let N = postorder.length;
         
     for (let i = 0; i < N; ++i) {
-        Indexer.set(inorder[i], i);
+        Indexer[inorder[i]] = i;
     }
     
     return Recursive(0, N - 1);
@@ -27,7 +27,7 @@ var buildTree = function(inorder, postorder) {
         } 
         
         let root = postorder.pop();
-        let rootIndex = Indexer.get(root);
+        let rootIndex = Indexer[root];
 
         let right = Recursive(rootIndex + 1, ie);
         let left = Recursive(is, rootIndex - 1)
