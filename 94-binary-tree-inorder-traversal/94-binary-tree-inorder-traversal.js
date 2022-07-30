11cd307df9cb15423088d1fcc97668f27b26a1a1
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-07-30 14:23:32 +0900

    Time: 85 ms (55.17%), Space: 42.1 MB (68.73%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  let path = [];
+  
+  DFS(root);
+  
+  return path;
+
+  function DFS(node) {
+    if (node === null) {
+      return;
+    }
+    
+    DFS(node.left);
+    path.push(node.val);
+    DFS(node.right); 
+  }    
+};
