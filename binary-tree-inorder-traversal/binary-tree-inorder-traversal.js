86d7bc2731ae66ebdbfc53e9be940ed055c31ba3
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-07-30 14:22:26 +0900

    Time: 123 ms, Memory: 42.6 MB - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -11,46 +11,19 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    /*
-        1. 노드의 left, right가 모두 있는 경우
-        2. 노드의 left만 있는 경우
-        3. 노드의 right만 있는 경우
-        4. 노드의 left, right가 모두 없는 경우
+  let path = [];
+  
+  DFS(root);
+  
+  return path;
 
-        1,2 => left를 스택에 추가 
-        3 => 현재 노드를 path에 추가하고 스택에서 제거, right를 스택에 추가
-        4 => 현재 노드를 path에 추가하고 스택에서 제거 => 상위 노드로 이동 => 상위 노드는 1,2인 상태지만 left가 없는 상태로 취급해줘야 함(즉, 3,4로 취급되어야 함)
-    */ 
-
-    let stack = [];
-    let path = [];
-    let flag = true;
-    let curNode = root;
-
-    while (curNode !== null || stack.length > 0) {
-        if (curNode !== null) {
-            stack.push(curNode);
-            curNode = curNode.left; 
-        } else {
-            curNode = stack.pop();
-            path.push(curNode.val);
-            curNode = curNode.right; 
-        }
+  function DFS(node) {
+    if (node === null) {
+      return;
     }
     
-    return path;
-    /*
-    if (root === null) {
-        return []; 
-    }
-    
-    if (root.left === null && root.right === null) {
-        return [root.val];
-    }
-    
-    let left = inorderTraversal(root.left);
-    let right = inorderTraversal(root.right);
-    
-    return [...left, root.val, ...right];
-    */
+    DFS(node.left);
+    path.push(node.val);
+    DFS(node.right); 
+  }    
 };
