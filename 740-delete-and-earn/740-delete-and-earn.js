a4d904082fec12d5dce0748c178742ee8a21aa45
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-05 09:30:09 +0900

    Time: 98 ms (72.03%), Space: 47.3 MB (22.32%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.js
+++ 740-delete-and-earn/740-delete-and-earn.js
@@ -4,55 +4,25 @@
  */
 var deleteAndEarn = function(nums) {
   const N = nums.length;
-  let ScoreMap = new Map();  
-  let ArrToSet= new Set(nums);
-  let Nums = Array.from(ArrToSet);
+  let Score = new Map();
+  let M = 0;
+
+  for (let num of nums) {
+    Score.has(num) ? 
+      Score.set(num, Score.get(num) + num) : 
+      Score.set(num, num);
+    M = Math.max(M, num);
+  }
+  
   let dp = new Map();
   
-  Nums.sort((a, b) => a - b);
+  dp.set(0, 0);
+  dp.set(1, Score.has(1) ? Score.get(1) : 0);
   
-  const M = Nums.length;
-
-  for (let i = 0; i < N; ++i) {
-    if (ScoreMap.has(nums[i])) {
-      ScoreMap.set(nums[i], ScoreMap.get(nums[i]) + nums[i]);
-      continue;
-    } 
-    
-    ScoreMap.set(nums[i], nums[i]);
-  } 
-  
-  return Recursion(Nums.at(-1));
-  
-  function Recursion(num) {
-    if (num === 0) {
-      return 0;
-    }
-    
-    if (num === 1) {
-      if (ScoreMap.has(1)) {
-        return ScoreMap.get(1);
-      } else {
-        return 0;
-      }
-    }
-    
-    if (dp.has(num)) {
-      return dp.get(num);
-    }
-    
-    let notSelect, select;
-    
-    notSelect = Recursion(num - 1);
-    
-    if (ScoreMap.has(num)){
-      select = Recursion(num - 2) + ScoreMap.get(num);
-    } else {
-      select = Recursion(num - 2);
-    }
-    
-    dp.set(num, Math.max(select, notSelect));
-    
-    return dp.get(num);
+  for (let i = 2; i <= M; ++i) {
+    let score = Score.has(i) ? Score.get(i) : 0;
+    dp.set(i, Math.max(dp.get(i - 2) + score, dp.get(i - 1)));
   }
+  
+  return dp.get(M);
 };
