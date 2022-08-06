22de537a17c507f692f2270f09cdc3d452af9d52
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-06 22:45:18 +0900

    Time: 80 ms, Memory: 44.1 MB - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -2,16 +2,16 @@
  * @param {number[]} cost
  * @return {number}
  */
+// dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
 var minCostClimbingStairs = function(cost) {
   cost.push(0);
   const N = cost.length;    
   let dp = new Array(N);
-  
   dp[0] = cost[0];
   dp[1] = cost[1];
- 
+  
   for (let i = 2; i < N; ++i) {
-    dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];  
+    dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
   }
   
   return dp[N - 1];
