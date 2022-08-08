8db7e945bf51e640b1d614b2c55358a6a37f5c89
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-08 18:00:30 +0900

    Time: 521 ms, Memory: 85.6 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.js
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.js
@@ -8,6 +8,20 @@ var maximumScore = function(nums, multipliers) {
   const M = multipliers.length;
   const N = nums.length;
   
+  let dp = new Array(M + 1).fill().map(() => new Array(M + 1).fill(0));
+  
+  for (let depth = M - 1; depth >= 0; --depth) {
+    for (let sIndex = 0; sIndex <= M - 1; ++sIndex) {
+      dp[depth][sIndex] = Math.max(
+        dp[depth + 1][sIndex] + nums[N - 1 - (depth - sIndex)] * multipliers[depth],
+        dp[depth + 1][sIndex + 1] + nums[sIndex] * multipliers[depth]
+      )
+    }
+  }
+  
+  return dp[0][0];
+
+  /*
   let dp = new Array(M).fill().map(() => new Array(M).fill(0));
   
   return Dynamic(0, 0);
@@ -27,5 +41,5 @@ var maximumScore = function(nums, multipliers) {
 
     return dp[depth][sIndex];
   }
-  
+  */ 
 };
