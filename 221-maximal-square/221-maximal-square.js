b3789a22c6a020e70f5b55e7219c6376bff5f897
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-10 10:38:33 +0900

    Time: 159 ms (38.20%), Space: 54.7 MB (8.24%) - LeetHub

--- 221-maximal-square/221-maximal-square.js
+++ 221-maximal-square/221-maximal-square.js
@@ -6,27 +6,25 @@ var maximalSquare = function(matrix) {
   const m = matrix.length,
         n = matrix[0].length;
   
-  let dp = new Array(m + 1).fill().map(() => new Array(n + 1).fill(0));
+  let dp = new Array(m + 1).fill(0);
+  let temp = 0;
+  let save;
+  let ans = 0;
 
-  for (let row = 1; row <= m; ++row) {
-    for (let col = 1; col <= n; ++col) {
-      if (matrix[row - 1][col - 1] === "1") {
-        dp[row][col] = 1 + Math.min(
-          dp[row - 1][col], 
-          dp[row][col - 1], 
-          dp[row - 1][col - 1]
-        );
+  for (let col = 0; col < n; ++col) {
+    for (let row = 1; row <= m; ++row) {
+      if (matrix[row - 1][col] === "1") {
+        save = dp[row];
+        dp[row] = Math.min(dp[row], dp[row - 1], temp) + 1;
+        ans = Math.max(ans, dp[row]);
+        temp = save;
+      } else {
+        save = dp[row];
+        dp[row] = 0;
+        temp = save;
       }  
     }
-  }
-  
-  let ans = 0;
-  
-  
-  for (let row = 1; row <= m; ++row) {
-    for (let col = 1; col <= n; ++col) {
-      ans = Math.max(ans, dp[row][col]);
-    }
+    console.log(dp);
   }
   
   return ans * ans;
