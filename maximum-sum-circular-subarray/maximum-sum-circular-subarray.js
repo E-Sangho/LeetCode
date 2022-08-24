309cf4d8814effe81526a3a90032df07f1873df3
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-24 16:05:01 +0900

    Time: 172 ms, Memory: 50.6 MB - LeetHub

--- maximum-sum-circular-subarray/maximum-sum-circular-subarray.js
+++ maximum-sum-circular-subarray/maximum-sum-circular-subarray.js
@@ -3,15 +3,34 @@
  * @return {number}
  */
 /*
- * Let dp[i] as maximum sum that ends as ith nums.
+ * Let dp[i] as maximum sum that ends at ith nums.
  * That means dp[i] := max_{j} nums[j] + nums[j + 1] + ... + nums[i].
  * Here nums is a circular array so j's range is 0 <= j < i || i + 2 <= j
- * When 0 <= j < i, we can use Kadane's Algorithm to solve this problem.
- * Other case, however, we can't use it, because we don't know where to start.
+ * Accordingly there are two possible cases to calculate it.
+ * First is max_{j} nums[j] + ... + nums[i], where 0 <= j < i.
+ * Second is max_{j} nums[j] + ... nums[N - 1] + nums[0] + ... + nums[i], where i + 2 <= j.
+ * Previous one is easily solved by Kadane's Algorithm.
+ * Let this answer as Front[i] = max_{j} nums[j] + ... + nums[i], where 0 <= j < i.
+ *
+ * Other case, however, we can't use it.
+ * If we start from j to i, where i + 2 <= j, 
+ * subarray sum should be nums[j] + nums[j + 1] + ... + nums[N - 1] + nums[0] + ... + nums[i].
+ * In this case nums[0] + ... nums[i] is fixed one, because we start from j, where i + 2 <= j.
+ * We want to find maximum subarray, so we have to check max_{j} nums[j] + ... nums[N - 1].
  *
  * Let A[i] := nums[0] + nums[1] + ... + nums[i],
- *     B[i] := nums[i] + nums[i + 1] + ... nums[N - 1], where N = nums.length.
- *     C(i, j) := A[i] + B[j]
+ *     B[i] := max_{j} nums[j] + nums[j + 1] + ... nums[N - 1], where i + 2 <= j,
+ *     Back[i] := A[i] + B[i] (This is maximum sum start from j, where i + 2 <= j)
+ *
+ * Finally we can find dp[i] to combine results.
+ * dp[i] = max(Front[i], Back[i])
+ *
+ * 1. use Kadane's Algorithm to find Front[i]
+ * 2. Find A[i]
+ * 3. Find B[i]
+ * 4. Find Back[i] from A[i] and B[i]
+ * 5. Find dp[i] = max(Front[i], Back[i])
+ * 6. return ans = max_{i} dp[i]
  */
 var maxSubarraySumCircular = function(nums) {
   const N = nums.length;
@@ -22,19 +41,24 @@ var maxSubarraySumCircular = function(nums) {
   
   A[0] = nums[0];
 
+  // use Kadane's Algorithm
   for (let i = 1; i < N; ++i) {
     cur = Math.max(nums[i], cur + nums[i]);
     best = Math.max(best, cur);
+    // Find A[i]
     A[i] = nums[i] + A[i - 1];  
   }
 
   let backCur = 0, backBest = 0;
   
   for (let i = N - 1; i >= 2; --i) {
+    // Find B[i]
     backCur += nums[i];
-    backBest = Math.max(backBest, backCur);
+    backBest = Math.max(backBest, backCur); // This is B[i]. We don't use it again, 
+                                            // so there is no reason to memorize it.
     
-    best = Math.max(best, A[i - 2] + backBest);
+    best = Math.max(best, A[i - 2] + backBest); // When we update best, we already checked all Front[i].
+                                                // Therefore we iteratively check Back[i] with best.
   }
 
   return best;
