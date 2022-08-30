a31892482579ae9c52f7808a719e555430ee51f6
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-30 10:16:35 +0900

    Time: 71 ms, Memory: 44.6 MB - LeetHub

--- domino-and-tromino-tiling/domino-and-tromino-tiling.js
+++ domino-and-tromino-tiling/domino-and-tromino-tiling.js
@@ -19,25 +19,26 @@
  * dp[1][2] = 2
  *
  * ans = dp[n - 1][2]
+ *
+ * +) 일반성을 잃지 않고 dp[i][0] = dp[i][1]인 것을 알 수 있다.
+ *    그러므로 
  */
 var numTilings = function(n) {
   const MOD = 1000000007;
-  let dp = new Array(n).fill().map(() => new Array(3).fill(0));
+  let dp = new Array(n).fill().map(() => new Array(2).fill(0));
   
   if (n <= 2) {
     return n;
   }
   
-  dp[0][2] = 1;
+  dp[0][1] = 1;
   dp[1][0] = 1;
-  dp[1][1] = 1;
-  dp[1][2] = 2;
+  dp[1][1] = 2;
     
   for (let i = 2; i < n; ++i) {
-    dp[i][0] = (dp[i - 1][1] + dp[i - 2][2]) % MOD;
-    dp[i][1] = (dp[i - 1][0] + dp[i - 2][2]) % MOD;
-    dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 2][2]) % MOD;
+    dp[i][0] = (dp[i - 1][0] + dp[i - 2][1]) % MOD;
+    dp[i][1] = (dp[i - 1][0] * 2 + dp[i - 1][1] + dp[i - 2][1]) % MOD;
   }
   
-  return dp[n - 1][2];
+  return dp[n - 1][1];
 };
