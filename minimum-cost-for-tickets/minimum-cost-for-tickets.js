e1efd434760a0295ef33eff8e15dfd39d59e5b11
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2022-08-31 18:04:47 +0900

    Time: 103 ms, Memory: 43 MB - LeetHub

---
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} days
+ * @param {number[]} costs
+ * @return {number}
+ *
+ * i번째 날에 1일 티켓 : dp[i] =  dp[i - 1] + costs[0]
+ * i번째 날에 7일 티켓 : dp[i] =  dp[i - 7] + costs[1]
+ *                   dp[i] = dp[i - 1] ... = dp[i - 6] 
+ * i번째 날에 30일 티켓 : dp[i] = dp[i - 30] + costs[2]
+ *                    dp[i] = dp[i - 1] ... = dp[i - 29]
+ */
+var mincostTickets = function(days, costs) {
+  const lastDay = days.at(-1);
+  const daySet = new Set(days);
+  
+  let dp = new Array(366);
+
+  return TopDown(1);  
+  
+  function TopDown(day) {
+    if (day > 365) {
+      return 0;
+    }
+    
+    if (dp[day] !== undefined) {
+      return dp[day];
+    }
+    
+    if (daySet.has(day)) {
+
+      dp[day] = Math.min(TopDown(day + 1) + costs[0], 
+                         TopDown(day + 7) + costs[1], 
+                         TopDown(day + 30)+ costs[2]);
+    } else {
+      dp[day] = TopDown(day + 1);
+    }
+    
+    return dp[day];
+  }
+};
