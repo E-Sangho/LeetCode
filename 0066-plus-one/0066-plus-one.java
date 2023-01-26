fa9c46a56fbac853281feb92701920bb03c63630
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-01-26 17:46:41 +0900

    Time: 0 ms (100.00%), Space: 42.4 MB (26.23%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -4,22 +4,21 @@ class Solution {
         digits[index] += 1;
         
         while (index >= 0) {
-            if (digits[index] == 10) {
-                digits[index] = 0;
-                if (index == 0) {
-                    int[] newDigits = new int[digits.length + 1];
-                    newDigits[0] = 1;
-                    for (int i = 0; i < digits.length; ++i) {
-                        newDigits[i + 1] = digits[i];
-                    }
-                    digits = newDigits;
-                } else {
-                    digits[index - 1] += 1;
-                }
-                --index;
-            } else {
+            if (digits[index] < 10) {
                 break;
-            }     
+            }
+            digits[index] = 0;
+            if (index == 0) {
+                int[] newDigits = new int[digits.length + 1];
+                newDigits[0] = 1;
+                for (int i = 0; i < digits.length; ++i) {
+                    newDigits[i + 1] = digits[i];
+                }
+                digits = newDigits;
+            } else {
+                digits[index - 1] += 1;
+            }
+            --index;
         }
         
         return digits;
