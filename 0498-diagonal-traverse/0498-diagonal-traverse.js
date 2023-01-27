81bc4b924b65b57c31c3eb6573824b558b96d958
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-01-27 16:57:51 +0900

    Time: 148 ms (38.57%), Space: 46.9 MB (98.21%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.js
+++ 0498-diagonal-traverse/0498-diagonal-traverse.js
@@ -3,35 +3,47 @@
  * @return {number[]}
  */
 var findDiagonalOrder = function(mat) {
-    let row = 0,
-        col = 0,
-        index = 0,
-        m = mat.length,
+    let m = mat.length,
         n = mat[0].length;
-    let memo = new Array(),
-        result = new Array();
     
-    for (let d = 0; d < m * n; ++d) {
-        if (d < n) {
-            row = 0;
-            col = d;
+    let isGoingUp = true;
+    let row = 0, col = 0;
+    let result = new Array(m * n);
+    let index = 0;
+    
+    while (row < m && col < n) {
+        result[index++] = mat[row][col];
+        if (isGoingUp) {
+            if (col === n - 1) {
+                ++row;
+                isGoingUp = false;
+                continue;
+            }
+                
+            if (row === 0) {
+                ++col;
+                isGoingUp = false;
+                continue;
+            }
+            
+            --row;
+            ++col;
         } else {
-            row = d - (n - 1);
-            col = n - 1;
-        }
-        
-        while (row < m && 0 <= col) {
-            memo.push(mat[row][col]);
+           if (row === m - 1) {
+               ++col;
+               isGoingUp = true;
+               continue;
+           } 
+            
+            if (col === 0) {
+                ++row;
+                isGoingUp = true;
+                continue;
+            }
+            
             ++row;
             --col;
         }
-        
-        if (d % 2 === 0) {
-            memo.reverse();
-        }
-        result = result.concat(memo);
-        memo = [];
     }
-    
     return result;
 };
