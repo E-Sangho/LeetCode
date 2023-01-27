031204f2f4b08656841b2480d3b92ea67e3748d3
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-01-27 17:42:45 +0900

    Time: 101 ms (70.85%), Space: 48.6 MB (40.36%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.js
+++ 0498-diagonal-traverse/0498-diagonal-traverse.js
@@ -3,47 +3,30 @@
  * @return {number[]}
  */
 var findDiagonalOrder = function(mat) {
-    let m = mat.length,
-        n = mat[0].length;
+   let m = mat.length,
+       n = mat[0].length;
     
-    let isGoingUp = true;
-    let row = 0, col = 0;
+    let arr = Array.from({ length: m + n - 1}, () => new Array());
     let result = new Array(m * n);
     let index = 0;
     
-    while (row < m && col < n) {
-        result[index++] = mat[row][col];
-        if (isGoingUp) {
-            if (col === n - 1) {
-                ++row;
-                isGoingUp = false;
-                continue;
-            }
-                
-            if (row === 0) {
-                ++col;
-                isGoingUp = false;
-                continue;
-            }
-            
-            --row;
-            ++col;
-        } else {
-           if (row === m - 1) {
-               ++col;
-               isGoingUp = true;
-               continue;
-           } 
-            
-            if (col === 0) {
-                ++row;
-                isGoingUp = true;
-                continue;
-            }
-            
-            ++row;
-            --col;
+    for (let row = 0; row < m; ++row) {
+        for (let col = 0; col < n; ++col) {
+            arr[row + col].push(mat[row][col]);
         }
     }
+    
+    for (let d = 0; d < m + n - 1; ++d) {
+        if (d % 2 == 0) {
+            for (let i = arr[d].length - 1; i >= 0; --i) {
+                result[index++] = arr[d][i];
+            } 
+        } else {
+            for (let i = 0; i < arr[d].length; ++i) {
+                result[index++] = arr[d][i];
+            } 
+        }
+    }
+    
     return result;
 };
