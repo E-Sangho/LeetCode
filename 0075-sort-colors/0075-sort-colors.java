38625a5b4c940cfb92c67bbcdbdc5f353910f250
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-02-21 09:23:22 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (72.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,24 +1,48 @@
 class Solution {
-  public void sortColors(int[] nums) {
-    selectionSort(nums);  
-  }
+    public void sortColors(int[] nums) {
+        countingSort(nums);  
+    }
     
-  public void selectionSort(int[] nums) {
-    int n = nums.length;
-    for (int i = 0; i < n - 1; ++i) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (nums[minIndex] > nums[j]) {
-                minIndex = j;
-            } 
+    public void selectionSort(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; ++i) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                } 
+            }
+            swap(nums, minIndex, i);
+        }    
+    }
+    
+    public void countingSort(int[] nums) {
+        int N = 3;
+        int[] counting = new int[N];
+        int[] ans = new int[nums.length];
+        
+        for (int num : nums) {
+            counting[num]++;
         }
-        swap(nums, minIndex, i);
-    }    
-  }
+        
+        for (int i = 1; i < N; ++i) {
+            counting[i] += counting[i - 1];    
+        }
+        
+        
+        for (int num : nums) {
+            ans[counting[num] - 1] = num;     
+            counting[num]--;
+        }
+        
+        for (int i = 0; i < ans.length; ++i) {
+            nums[i] = ans[i];
+        }
+    }
     
-  public void swap(int[] nums, int i, int j) {
-    int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
