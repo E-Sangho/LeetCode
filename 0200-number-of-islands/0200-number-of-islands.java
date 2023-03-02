d6e8ae006abae24376af89870a05cdd2b1456c82
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-03-02 10:39:59 +0900

    Time: 11 ms (12.54%), Space: 50 MB (92.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,74 +1,66 @@
 class Solution {
-    // declare global variables
+    final char WATER = '0',
+               ISLAND = '1';
     
-    // directions of adjacent lands
-    final int direction[][] = new int[][]{
-        {-1, 0},
-        {0, 1},
-        {1, 0},
-        {0, -1},
+    final int[][] directions = new int[][] {
+        { -1, 0 },
+        { 0, 1 },
+        { 1, 0 },
+        { 0, -1 }
     };
-    
-    // make constant variables for water and island 
-    final char ISLAND = '1',
-               WATER = '0';
     
     public int numIslands(char[][] grid) {
         int m = grid.length,
             n = grid[0].length,
-            total = 0;
+            count = 0;
         
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
-                if (grid[r][c] != WATER) {
-                    BFS(grid, r, c); 
-                    ++total;
-                }    
-            }
-        }        
-        
-        return total;
-    }
-    
-    // bfs search
-    public void BFS(char[][] grid, int r, int c) {
-        int m = grid.length,
-            n = grid[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        
-        queue.add(new int[] {r, c});
-        grid[r][c] = WATER;
-        
-        while (queue.size() > 0) {
-            int curSize = queue.size();
-            for (int i = 0; i < curSize; ++i) {
-                int[] front = queue.poll();
-                int row = front[0],
-                    col = front[1];
-                for (int d = 0; d < 4; ++d) {
-                    int nextRow = row + direction[d][0],
-                        nextCol = col + direction[d][1];
-                    if (isInEarth(nextRow, nextCol, m, n) && grid[nextRow][nextCol] == ISLAND) {
-                        grid[nextRow][nextCol] = WATER;
-                        queue.add(new int[]{ nextRow, nextCol });
-                    }
+                if (grid[r][c] == ISLAND) {
+                    DFS(grid, r, c); 
+                    ++count; 
                 }
             }
         }
+        
+        return count;
     }
     
-    // check (r, c) is in grid or not
+    public void DFS(char[][] grid, int r, int c) {
+        int m = grid.length,
+            n = grid[0].length;
+        
+        Stack<int[]> stack = new Stack<>();     
+        stack.push(new int[] { r, c });
+        
+        while (stack.size() > 0) {
+            int[] front = stack.pop();
+            int row = front[0],
+                col = front[1];
+            
+            for (int d = 0; d < 4; ++d) {
+                int nextRow = row + directions[d][0],
+                    nextCol = col + directions[d][1];
+                
+                if (isInEarth(nextRow, nextCol, m, n) && grid[nextRow][nextCol] == ISLAND) {
+                    grid[nextRow][nextCol] = WATER;
+                    stack.push(new int[] { nextRow, nextCol });
+                }
+            }
+        }
+    } 
+    
     public boolean isInEarth(int r, int c, int m, int n) {
         if (
             0 <= r &&
-            r < m &&
+            r < m  &&
             0 <= c &&
-            c < n
+            c < n  
         ) {
             return true;
         }
         
         return false;
     }
+    
 }
