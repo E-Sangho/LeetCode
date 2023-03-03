54cab070645eee571b8fc3279c41c4a63b11970f
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-03-03 13:37:18 +0900

    Time: 301 ms (39.11%), Space: 42.7 MB (23.72%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,28 +1,43 @@
 class Solution {
+    /*
+     * dp[i][sum] := 0 ~ i까지 인덱스까지 합이 sum인 경우의 수
+     * dp[i][x] = dp[i - 1][x - nums[i]] + dp[i - 1][x + nums[i]]
+     */
+
     int[] dfsNums;
     int dfsTarget;
     int N;
     int count;
+    int[][] dp;
+    int total;
     
     public int findTargetSumWays(int[] nums, int target) {
         dfsNums   = nums;    
         dfsTarget = target;
         N = nums.length;
         count = 0;
-        DFS(0, 0);
-        
-        return count;
+        total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        dp = new int[N][2 * total + 1];
+        return DFS(0, 0);
     }
     
-    public void DFS(int sum, int index) {
+    public int DFS(int sum, int index) {
         if (index == N) {
             if (sum == dfsTarget) {
-                ++count;
+                return 1;
             }
-            return;
+            return 0;
         } 
         
-        DFS(sum + dfsNums[index], index + 1);
-        DFS(sum - dfsNums[index], index + 1);
+        if (dp[index][sum + total] != 0) {
+            return dp[index][sum + total];
+        }
+        
+        dp[index][total + sum] = DFS(sum + dfsNums[index], index + 1) + DFS(sum - dfsNums[index], index + 1);
+        
+        return dp[index][total + sum];
     }
 }
