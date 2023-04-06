c175a4ca1ed272ac6b70ee544731bcca63dca612
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-04-06 09:30:16 +0900

    Time: 0 ms (100.00%), Space: 39.6 MB (39.38%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.java
+++ 0276-paint-fence/0276-paint-fence.java
@@ -4,19 +4,16 @@ class Solution {
      * dp[i] = (k - 1) * dp[i - 1] + (k - 1) * dp[i - 2]
      */ 
     public int numWays(int n, int k) {
-        int[] dp = new int[n + 1];
-        
-        if (n == 1) {
-            return k;
+        int[] dp = new int[n+1];
+        if (n <= 2) return (int)Math.pow(k, n);
+        dp[0] = 1;
+        dp[1] = k;
+        dp[2] = k*k;
+        dp[3] = k*dp[2] - k;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i-1]*k -(k-1)*dp[i-3];
         }
-        
-        dp[0] = k;
-        dp[1] = k * k;
-        
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = (k - 1) * dp[i - 1] + (k - 1) * dp[i - 2];
-        }
-        
-        return dp[n - 1];
+//        System.out.println(Arrays.toString(dp));
+        return dp[n];
     }
 }
