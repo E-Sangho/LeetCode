13d5006289f043ce8612ffefc205bbec341167e1
Sangho Lee <77875967+E-Sangho@users.noreply.github.com>
2023-04-13 23:09:37 +0900

    Time: 28 ms (59.37%), Space: 50.6 MB (32.50%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,11 +5,25 @@ class Solution {
      */
     private int[][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for (int i = 0; i < text1.length(); ++i) {
+            for (int j = 0; j < text2.length(); ++j) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        
+        return dp[text1.length()][text2.length()];
+        /*
         dp = new int[text1.length()][text2.length()];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
         return Recursive(text1.length() - 1, text2.length() - 1, text1, text2);
+        */
     }
     
     private int Recursive(int i, int j, String text1, String text2) {
